8941fddb3e2f77e58bd5953794f2e2b673e92af2
whoisYoges <helloyogesh@tuta.io>
2022-07-15 14:44:24 +0545

    Better error handling using loops

--- files/post_install.sh
+++ files/post_install.sh
@@ -63,7 +63,8 @@ timezone() {
     echo "Getting Timezone..."
     time_zone="$(curl --fail https://ipapi.co/timezone)"
     clear
-    echo "System detected your timezone to be $time_zone"
+    echo "System detected your timezone to be $time_zone."
+    echo "Incase automatic detection fails, go with no. 2 and enter the timezone manually"
     echo "Is this correct?"
 
     PS3="Select one.[1/2]: "
@@ -71,16 +72,30 @@ timezone() {
     select one in "${options[@]}"; do
         case $one in
             Yes)
-                echo "${time_zone} set as timezone"
-                ln -sf /usr/share/zoneinfo/"$time_zone" /etc/localtime && break
+                echo "$time_zone set as timezone"
+                ln -sf "/usr/share/zoneinfo/${time_zone}" /etc/localtime && break
                 ;;
             No)
-                echo "Please enter your desired timezone e.g. Europe/London :"
+                echo "Please enter your desired timezone e.g. Asia/Kathmandu or Europe/London (Case Sensative): "
                 read -r new_timezone
-                echo "${new_timezone} set as timezone"
-                ln -sf /usr/share/zoneinfo/"$time_zone" /etc/localtime && break
+                echo "Checking timezone $new_timezone ..."
+                sleep 2s
+                if ! ls "/usr/share/zoneinfo/${new_timezone}"
+                then
+                  echo "Timezone ${new_timezone} doesnot exist."
+                  sleep 2s
+                else  
+                  echo "Timezone ${new_timezone} found. Setting it up..."
+                  sleep 2s
+                  ln -sf "/usr/share/zoneinfo/${new_timezone}" /etc/localtime && break
+                fi
+                sleep 2s
                 ;;
-            *) echo "Wrong option. Try again";timezone;;
+            *)
+            echo "Wrong option. Try again..."
+            sleep 2s
+            timezone
+            ;;
         esac
     done
 }
@@ -112,7 +127,7 @@ settingKeyboard() {
     echo "Setting console keyboard layout"
     echo "KEYMAP=us" > /etc/vconsole.conf
     echo "Checking system Keyboard Layout..."
-    cat /etc/locale.conf || ignoreableErrors
+    cat /etc/vconsole.conf || ignoreableErrors
     sleep 3s
 }
 
@@ -181,10 +196,10 @@ loginManager(){
 
 installingLoginManager() {
   s=0
-  for ((i=1;i<=5;i++)); do
+  for ((i=1;i<=4;i++)); do
     if ! loginManager; then
       ((s++))
-      if ((s==5)); then
+      if ((s==4)); then
         echo "Login manager installation failed..."
         ignoreableErrors
         sleep 3s
@@ -246,7 +261,7 @@ copyingConfig() {
 }
 
 installingPackages() {
-    pacman -Sy --needed --noconfirm metis-dwm metis-st metis-dmenu metis-wallpapers xorg-server xorg-xinit xorg-xsetroot nerd-fonts-jetbrains-mono ttf-font-awesome pavucontrol pulseaudio pulseaudio-alsa firefox brillo linux-zen-headers linux-firmware curl git neovim zsh metis-slstatus picom-jonaburg-git acpi xwallpaper libxft-bgra|| installationError
+    pacman -Sy --needed --noconfirm metis-dwm metis-st metis-dmenu metis-wallpapers xorg-server xorg-xinit xorg-xsetroot nerd-fonts-jetbrains-mono ttf-font-awesome pavucontrol pulseaudio pulseaudio-alsa firefox brillo git neovim metis-slstatus picom-jonaburg-git acpi xwallpaper libxft-bgra
 }
 
 packagesCompleted() {
@@ -281,50 +296,58 @@ installingMicrocode() {
     fi
 }
 
-configuringBootloader() {
-    sleep 2s
-    clear
-    if [[ ! -d "/sys/firmware/efi" ]]; then
-      echo "Legacy system detected..."
-      echo "Enter the drive name to install bootloader in it. (eg: sda or nvme01 or vda or something similar)! "
-      echo "NOTE: JUST GIVE DRIVE NAME (sda, nvme0n1, vda or something similar); NOT THE PARTITION NAME (sda1)"
-      echo "Enter the drive name: "
-      read -r grubdrive
-      grub-install --target=i386-pc /dev/"$grubdrive"
-      grub-mkconfig -o /boot/grub/grub.cfg
-    else
-      echo "UEFI system detected..."
-      echo "Installing efibootmgr"
-      pacman -Sy --needed --noconfirm efibootmgr dosfstools
-      sleep 2s
-      clear
-      lsblk
-      echo "Enter partition to install grub! (eg: sda2 or nvme0n1p3, vda4 or something similar): "
-      read -r grubpartition
-      mkfs.fat -F 32 /dev/"$grubpartition"
-      mkdir -p /boot/efi
-      mount /dev/"$grubpartition" /boot/efi
-      grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=metis --removable
-      grub-mkconfig -o /boot/grub/grub.cfg
-    fi
-    sleep 2s
+selectDrive() {
+  lsblk
+  echo "Enter the drive name to install bootloader in it. (eg: sda or nvme01 or vda or something similar)! "
+  echo "NOTE: JUST GIVE DRIVE NAME (sda, nvme0n1, vda or something similar); NOT THE PARTITION NAME (sda1)"
+  echo "Enter the drive name: "
+  read -r grubdrive
 }
 
-bootloaderconfigCompleted() {
+checkDrive() {
+  while ! lsblk | grep "$grubdrive"  &> /dev/null
+  do
+    clear
+    echo -e "$grubdrive drive is not found. Check your available disks in which you are installing metis os and try again..."
+    selectDrive
+  done
+}
+
+selectGrub() {
+  lsblk
+  echo "Enter the boot/grub installation partition (eg: sda1 or nvme0n1p2 or vda4): "
+  read -r grubpartition
+}
+
+checkGrub() {
+  while ! lsblk | grep "$grubpartition"  &> /dev/null
+  do
+    clear
+    echo -e "$grubpartition partition is not found. Check your available partitions and try again..."
+    selectGrub
+  done
+}
+
+uefiPackages() {
+  pacman -Sy --needed --noconfirm efibootmgr dosfstools
+}
+
+uefiCompleted() {
+  echo "UEFI system detected!"
+  echo "Installing efibootmgr and dosfstools..."
   s=0
-  for ((i=1;i<=3;i++)); do
-    if ! configuringBootloader; then
+  for ((i=1;i<=5;i++)); do
+    if ! uefiPackages; then
       ((s++))
-      if ((s==3)); then
-        echo "Failed to configure bootloader..."
-        failed
+      if ((s==5)); then
+        echo "Failed to install efibootmgr abd dosfstools..."
+        installationError
       fi
-        umount /dev/"$grubpartition"
         clear
-        echo "Failed to configure bootloader. Retrying..."
+        echo "Failed to install efibootmgr and dosfstools. Retrying..."
         sleep 2s
       else {
-        echo "Bootloader configuration successful..."
+        echo "Installation of efibootmgr and dosfstools successful..."
         sleep 2s
         break
       }
@@ -332,6 +355,38 @@ bootloaderconfigCompleted() {
   done
 }
 
+configuringBootloader() {
+    sleep 2s
+    clear
+    if [[ ! -d "/sys/firmware/efi" ]]; then
+      echo "Legacy system detected..."
+      selectDrive
+      checkDrive
+      grub-install --target=i386-pc /dev/"$grubdrive"
+      grub-mkconfig -o /boot/grub/grub.cfg
+    else
+      uefiCompleted
+      sleep 2s
+      clear
+      selectGrub
+      checkGrub
+      mkfs.fat -F 32 /dev/"$grubpartition" || failed
+      mkdir -p /boot/efi
+      mount /dev/"$grubpartition" /boot/efi
+      if mountpoint /boot/efi = "/boot/efi is not a mountpoint"; then
+        clear
+        echo -e "$grubpartition is not mounted successfully. Aborting installation!\nInstallation failed..."
+        exit 1
+      else
+        echo "Cool $grubpartition mounted at /boot/efi! Installing grub..."
+        sleep 2s
+      fi
+      grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=metis --removable || failed
+      grub-mkconfig -o /boot/grub/grub.cfg || failed
+    fi
+    sleep 2s
+}
+ 
 graphicsDriver() {
     clear
     echo "Searching and Installing graphics driver if available"
@@ -366,12 +421,12 @@ settingHostname
 settingHosts
 bootloaderCompleted
 enablingNetworkManager
-installingLoginManager
+# installingLoginManager
 addingUser
 sudoAccess
 copyingConfig
 packagesCompleted
 installingMicrocode
-bootloaderconfigCompleted
+configuringBootloader
 graphicsDriver
 secondphaseCompleted
